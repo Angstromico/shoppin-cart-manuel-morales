b407611080c683646a6a952c360ebb11715cbc00
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-10-18 00:57:36 -0400

    LocalStore

--- js/app.js
+++ js/app.js
@@ -1,57 +1,63 @@
 //Variables
-const cart = document.getElementById('cart')
-const containerCart = document.getElementById('list-cart')
-const emptyButton = document.getElementById('empty-cart')
-const coursesList = document.getElementById('list-courses')
-const form = document.querySelector('#Searching')
-let cartArticles = []
-const coursesTitles = document.querySelectorAll('h4')
+const cart = document.getElementById('cart');
+const containerCart = document.getElementById('list-cart');
+const emptyButton = document.getElementById('empty-cart');
+const coursesList = document.getElementById('list-courses');
+const form = document.querySelector('#Searching');
+let cartArticles = [];
+const coursesTitles = document.querySelectorAll('h4');
 const coursesArray = Array.from(coursesTitles).map(
   (titles) => titles.textContent
-)
-const searching = document.getElementById('searcher')
-let total = 0
-const hero = document.getElementById('hero')
+);
+const searching = document.getElementById('searcher');
+let total = 0;
+const hero = document.getElementById('hero');
 //Buy Button
-const buyButton = document.createElement('BUTTON')
-buyButton.setAttribute('type', 'button')
-buyButton.textContent = 'Buy Here'
-buyButton.classList.add('golden-button')
-cart.append(buyButton)
+const buyButton = document.createElement('BUTTON');
+buyButton.setAttribute('type', 'button');
+buyButton.textContent = 'Buy Here';
+buyButton.classList.add('golden-button');
+cart.append(buyButton);
 //Even Listeners
-eventListeners()
+eventListeners();
 function eventListeners() {
   //to add and remove course
-  coursesList.addEventListener('click', addCourse)
+  coursesList.addEventListener('click', addCourse);
   //Eliminated courses from the shopping Cart
-  cart.addEventListener('click', eraseCourses)
+  cart.addEventListener('click', eraseCourses);
   //Button to Empty the cart
   emptyButton.addEventListener('click', () => {
-    cartArticles = []
-    cleaningDOM()
-  })
+    cartArticles = [];
+    localStorage.removeItem('cart');
+    cleaningDOM();
+  });
+  //Show elements on localStorage
+  document.addEventListener('DOMContentLoaded', () => {
+    cartArticles = JSON.parse(localStorage.getItem('cart')) || [];
+    cartDOM();
+  });
 }
 //Functions
 function addCourse(e) {
-  e.preventDefault()
+  e.preventDefault();
   if (e.target.classList.contains('add-cart')) {
-    const courseSelected = e.target.parentElement.parentElement
-    readCourseData(courseSelected)
+    const courseSelected = e.target.parentElement.parentElement;
+    readCourseData(courseSelected);
   }
 }
 //Eliminate courses from the shopping cart
 function eraseCourses(e) {
   if (e.target.classList.contains('eraser')) {
-    const courseId = e.target.getAttribute('data-id')
+    const courseId = e.target.getAttribute('data-id');
     //total -= 15;
     //Vanish for the array
-    cartArticles = cartArticles.filter((course) => course.id !== courseId)
-    cartDOM()
+    cartArticles = cartArticles.filter((course) => course.id !== courseId);
+    cartDOM();
   }
 }
 //Take the HtML info for the Card of The Courses
 function readCourseData(course) {
-  console.log(course)
+  console.log(course);
   //Creating an Object that content  the course information
   const courseInfo = {
     image: course.querySelector('img').src,
@@ -60,39 +66,39 @@ function readCourseData(course) {
     price: course.querySelector('.price span').textContent,
     id: course.querySelector('a').getAttribute('data-id'),
     quantity: 1,
-  }
+  };
   //Review is an element already exist on the Shopping Cart
-  const exist = cartArticles.some((course) => course.id === courseInfo.id)
+  const exist = cartArticles.some((course) => course.id === courseInfo.id);
   if (exist) {
     //Update quantity
     const courses = cartArticles.map((course) => {
       if (course.id === courseInfo.id) {
-        course.quantity++
-        return course
+        course.quantity++;
+        return course;
       } else {
-        return course
+        return course;
       }
-    })
-    cartArticles = [...courses]
+    });
+    cartArticles = [...courses];
   } else {
     //App the course to the Shopping Cart
-    cartArticles = [...cartArticles, courseInfo]
+    cartArticles = [...cartArticles, courseInfo];
   }
   //Cleaning the Array cartArticles of previus elments
 
-  console.log(cartArticles)
-  cartDOM()
+  console.log(cartArticles);
+  cartDOM();
 }
 //Show the shopping car on the HTML
 function cartDOM() {
   //Cleaning the Last adition to shopping cart
-  cleaningDOM()
+  cleaningDOM();
   //Loop the cart and genetes the HTML
   cartArticles.forEach((course) => {
-    const { image, title, price, quantity, id } = course
-    const row = document.createElement('TR')
-    total = quantity * 15
-    hero.style.backgroundImage = `url(${image})`
+    const { image, title, price, quantity, id } = course;
+    const row = document.createElement('TR');
+    total = quantity * 15;
+    hero.style.backgroundImage = `url(${image})`;
     row.innerHTML = `
             <td>
                 <img src="${image}" class="shop-img">  
@@ -112,19 +118,24 @@ function cartDOM() {
             <td class="pay">
                 ${total}
             </td>
-        `
+        `;
     //Add the info to the tbody
-    containerCart.append(row)
+    containerCart.append(row);
     //Show buy button
-    const sumatorium = document.querySelectorAll('.pay')
+    const sumatorium = document.querySelectorAll('.pay');
     const totalToPay = Array.from(sumatorium).map((td) =>
       parseInt(td.textContent.trim())
-    )
-    const payTotal = totalToPay.reduce((a, b) => a + b)
-    console.log(payTotal)
-    buyButton.textContent = `Total to Pay: ${payTotal}`
-    console.log(buyButton)
-  })
+    );
+    const payTotal = totalToPay.reduce((a, b) => a + b);
+    console.log(payTotal);
+    buyButton.textContent = `Total to Pay: ${payTotal}`;
+    console.log(buyButton);
+  });
+  //Add Information to Local Store
+  storageSyn();
+}
+function storageSyn() {
+  localStorage.setItem('cart', JSON.stringify(cartArticles));
 }
 //Cleaning the Courses from tbody
 function cleaningDOM() {
@@ -132,35 +143,35 @@ function cleaningDOM() {
   //containerCart.innerHTML = '';
   //Efficient way
   while (containerCart.firstChild) {
-    containerCart.removeChild(containerCart.firstChild)
+    containerCart.removeChild(containerCart.firstChild);
   }
 }
 //Autocomplete bar
 $(function () {
-  coursesArray
+  coursesArray;
   $('#searcher').autocomplete({
     source: coursesArray,
-  })
-})
+  });
+});
 //Scroll to Search Bar
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const searchBar = searching.value.toLowerCase().trim()
-  const h4 = document.querySelectorAll('h4')
-  const courses = Array.from(h4)
-  const coursesLower = coursesArray.map((e) => e.toLowerCase())
+  e.preventDefault();
+  const searchBar = searching.value.toLowerCase().trim();
+  const h4 = document.querySelectorAll('h4');
+  const courses = Array.from(h4);
+  const coursesLower = coursesArray.map((e) => e.toLowerCase());
   for (let i = 0; i <= courses.length; i++) {
     if (searchBar == coursesLower[i]) {
-      const obj = courses[i]
+      const obj = courses[i];
       obj.scrollIntoView({
         behavior: 'smooth',
-      })
+      });
     }
   }
-})
+});
 //Set time intervals to change background image
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 const randomImages = [
   'https://miro.medium.com/max/1400/1*bcZz-qb_DNpvrNNwQBhQmQ.jpeg',
@@ -168,11 +179,11 @@ const randomImages = [
   'https://ubiqum.com/assets/uploads/2019/07/code-coder-coding-270348.jpg',
   'https://modeling-languages.com/wp-content/uploads/2019/03/php.png',
   'https://www.limestone.edu/sites/default/files/styles/de2e/public/images/2020-06/computer-programming.jpg?itok=2eSD1_GT',
-]
+];
 document.addEventListener('DOMContentLoaded', function () {
   setInterval(() => {
     hero.style.backgroundImage = `url(${
       randomImages[getRandomInt(0, randomImages.length - 1)]
-    })`
-  }, 25000)
-})
+    })`;
+  }, 25000);
+});
